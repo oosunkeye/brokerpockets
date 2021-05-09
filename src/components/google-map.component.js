3ed16e53aa2f740211b14104fd0b71a76d271041
Oladapo Osunkeye <davidosunkeye@gmail.com>
2021-05-09 18:03:17 -0400

    Update google-map.component.js

--- src/components/google-map.component.js
+++ src/components/google-map.component.js
@@ -8,21 +8,26 @@ import PlacesAutocomplete, {
 export class MapContainer extends Component {
 	constructor(props) {
 		super(props);
-		console.log(props.lat);
-		console.log("loaded...");
+
 		this.state = {
 			// for google map places autocomplete
 			address: "",
+			john: true,
 
 			showingInfoWindow: false,
 			activeMarker: {},
 			selectedPlace: {},
 
 			mapCenter: {
-				lat: 43.764115,
-				lng: -79.5058454,
+				lat: props.lat,
+				lng: props.lng,
 			},
+
+			latNew: props.lat,
+			lngNew: props.lng,
 		};
+
+		this.setState({ latNew: "props.lat" });
 	}
 
 	handleChange = (address) => {
@@ -43,94 +48,32 @@ export class MapContainer extends Component {
 			.catch((error) => console.error("Error", error));
 	};
 
+	componentDidUpdate(prevProps, prevState) {
+		if (prevProps.lat !== this.props.lat) {
+			console.log("google map state has changed.");
+			this.setState({ latNew: this.props.lat, lngNew: this.props.lng });
+		}
+	}
+
 	render() {
 		return (
 			<div id="googleMaps">
-				{/* <PlacesAutocomplete
-					value={this.state.address}
-					onChange={this.handleChange}
-					onSelect={this.handleSelect}
-				>
-					{({
-						getInputProps,
-						suggestions,
-						getSuggestionItemProps,
-						loading,
-					}) => (
-						<div>
-							<input
-								{...getInputProps({
-									placeholder: "Search Places ...",
-									className: "location-search-input",
-								})}
-							/>
-							<div className="autocomplete-dropdown-container">
-								{loading && <div>Loading...</div>}
-								{suggestions.map((suggestion) => {
-									const className = suggestion.active
-										? "suggestion-item--active"
-										: "suggestion-item";
-									// inline style for demonstration purpose
-									const style = suggestion.active
-										? { backgroundColor: "#fafafa", cursor: "pointer" }
-										: { backgroundColor: "#ffffff", cursor: "pointer" };
-									return (
-										<div
-											{...getSuggestionItemProps(suggestion, {
-												className,
-												style,
-											})}
-										>
-											<span>{suggestion.description}</span>
-										</div>
-									);
-								})}
-							</div>
-						</div>
-					)}
-				</PlacesAutocomplete> */}
-
-				{/* <Map
+				<Map
 					google={this.props.google}
 					style={{ width: "35%", height: "35%", position: "relative" }}
 					initialCenter={{
-						lat: this.props.lat,
-						lng: this.props.lng,
+						lat: this.state.latNew,
+						lng: this.state.lngNew,
 					}}
 					center={{
-						lat: this.props.lat,
-						lng: this.props.lng,
+						lat: this.state.latNew,
+						lng: this.state.lngNew,
 					}}
 				>
 					<Marker
 						position={{
-							lat: this.props.lat,
-							lng: this.props.lng,
-						}}
-					/>
-				</Map> */}
-
-				<Map
-					google={this.props.google}
-					style={{
-						width: "35%",
-						height: "36%",
-
-						position: "relative",
-					}}
-					initialCenter={{
-						lat: this.state.mapCenter.lat,
-						lng: this.state.mapCenter.lng,
-					}}
-					center={{
-						lat: this.state.mapCenter.lat,
-						lng: this.state.mapCenter.lng,
-					}}
-				>
-					<Marker
-						position={{
-							lat: this.state.mapCenter.lat,
-							lng: this.state.mapCenter.lng,
+							lat: this.state.latNew,
+							lng: this.state.lngNew,
 						}}
 					/>
 				</Map>
